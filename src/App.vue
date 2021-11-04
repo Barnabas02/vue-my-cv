dde9db3ad51eda21a3a13e0648003dba85f00485
Barnabas Tinarwo <barnabastinarwo@gmail.com>
2021-11-04 10:37:02 +0200

    Sample information added and template files added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- Spinner Start -->
+    <!-- <div
+      id="spinner"
+      class="
+        show
+        bg-white
+        position-fixed
+        translate-middle
+        w-100
+        vh-100
+        top-50
+        start-50
+        d-flex
+        align-items-center
+        justify-content-center
+      "
+    >
+      <div
+        class="spinner-border text-primary"
+        style="width: 3rem; height: 3rem"
+        role="status"
+      >
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div> -->
+    <!-- Spinner End -->
+
+    <div class="container">
+      <div class="row g-5">
+        <div class="col-lg-4 sticky-lg-top vh-100">
+          <div
+            class="d-flex flex-column h-100 text-center overflow-auto shadow"
+          >
+            <img
+              class="w-100 img-fluid mb-4"
+              src="img/profile.jpg"
+              alt="Image"
+            />
+            <h1 class="text-primary mt-2">John Doe</h1>
+            <div class="mb-4" style="height: 22px">
+              <h4 class="typed-text-output d-inline-block text-light"></h4>
+              <div class="typed-text d-none">
+                Web Designer, Web Developer, Front End Developer, Apps Designer,
+                Apps Developer
+              </div>
+            </div>
+            <div class="d-flex justify-content-center mt-auto mb-3">
+              <a class="btn btn-secondary btn-square mx-1" href="#"
+                ><i class="fab fa-twitter"></i
+              ></a>
+              <a class="btn btn-secondary btn-square mx-1" href="#"
+                ><i class="fab fa-facebook-f"></i
+              ></a>
+              <a class="btn btn-secondary btn-square mx-1" href="#"
+                ><i class="fab fa-linkedin-in"></i
+              ></a>
+              <a class="btn btn-secondary btn-square mx-1" href="#"
+                ><i class="fab fa-instagram"></i
+              ></a>
+            </div>
+            <div
+              class="d-flex align-items-end justify-content-between border-top"
+            >
+              <a href="" class="btn w-50 border-end">Download CV</a>
+              <a href="#contact" class="btn w-50 btn-scroll">Contact Me</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <About />
+          <Experience />
+          <Skills />
+          <References />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from "./components/About.vue";
+import Experience from "./components/Experience.vue";
+import Footer from "./components/Footer.vue";
+import References from "./components/References.vue";
+import Skills from "./components/Skills.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    About,
+    Experience,
+    Skills,
+    References,
+    Footer,
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/Experience.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="experience">
+    <section class="py-5 wow fadeInUp" data-wow-delay="0.1s">
+      <h1 class="title pb-3 mb-5">Expericence</h1>
+      <div class="border-start border-2 border-light pt-2 ps-5">
+        <div class="position-relative mb-4">
+          <span
+            class="bi bi-arrow-right fs-4 text-light position-absolute"
+            style="top: -5px; left: -50px"
+          ></span>
+          <h5 class="mb-1">Web Designer</h5>
+          <p class="mb-2">Soft Company | <small>2000 - 2050</small></p>
+          <p>
+            Tempor eos dolore amet tempor dolor tempor. Dolore ea magna sit amet
+            dolor eirmod. Eos ipsum est tempor dolor. Clita lorem kasd sed ea
+            lorem diam ea lorem eirmod duo sit ipsum stet lorem diam
+          </p>
+        </div>
+        <div class="position-relative mb-4">
+          <span
+            class="bi bi-arrow-right fs-4 text-light position-absolute"
+            style="top: -5px; left: -50px"
+          ></span>
+          <h5 class="mb-1">Web Designer</h5>
+          <p class="mb-2">Soft Company | <small>2000 - 2050</small></p>
+          <p>
+            Tempor eos dolore amet tempor dolor tempor. Dolore ea magna sit amet
+            dolor eirmod. Eos ipsum est tempor dolor. Clita lorem kasd sed ea
+            lorem diam ea lorem eirmod duo sit ipsum stet lorem diam
+          </p>
+        </div>
+        <div class="position-relative mb-4">
+          <span
+            class="bi bi-arrow-right fs-4 text-light position-absolute"
+            style="top: -5px; left: -50px"
+          ></span>
+          <h5 class="mb-1">Web Designer</h5>
+          <p class="mb-2">Soft Company | <small>2000 - 2050</small></p>
+          <p>
+            Tempor eos dolore amet tempor dolor tempor. Dolore ea magna sit amet
+            dolor eirmod. Eos ipsum est tempor dolor. Clita lorem kasd sed ea
+            lorem diam ea lorem eirmod duo sit ipsum stet lorem diam
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
